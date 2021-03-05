bb668f92422554debc3114e8c1fdf5d0640e7f21
WirelessAPI <57565189+aidelojep@users.noreply.github.com>
2021-03-05 04:44:29 +0100

    Add files via upload
    Just playing around with WHILE LOOP

---
+++ Loops.java
@@ -0,0 +1,23 @@
+
+ public class Loops{
+
+public static void main (String[] args){
+
+int a = 0;
+
+ while( a < 10) 
+     {
+  System.out.println("a");
+a++;
+}
+
+System.out.println("Loops has ended now!");
+
+}
+
+
+
+
+
+
+}
