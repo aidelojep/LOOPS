068dd4c4814e6c10a2c6e040ce2c59d0329c0e88
WirelessAPI <57565189+aidelojep@users.noreply.github.com>
2021-03-05 07:28:59 +0100

    Add files via upload
    Playing around with FOR Loop. It's quite simple compared to other loops..... I meannnnnnn......

---
+++ Loop.java
@@ -0,0 +1,23 @@
+public class Loop{
+ public static void main(String[] args) {
+
+for(int sarah=0; sarah < 100; sarah++)
+
+{
+
+System.out.println("For loops are bae, because they are quite simple!");
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
