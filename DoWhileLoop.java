c6d4302bb21008d541d45aa8a0a33c1aff5e8c21
WirelessAPI <57565189+aidelojep@users.noreply.github.com>
2021-03-05 08:04:40 +0100

    Add files via upload
    DoWhileLoop..... kinda interesting Loop, it just thrills me on why I would wanna  DO something before considering the condition. However, this is useful when a part of the program needs to be repeated

---
+++ DoWhileLoop.java
@@ -0,0 +1,16 @@
+public class DoWhileLoop{
+
+ public static void main(String[] args){
+
+     int a = 10;
+
+   do{ System.out.printf("Do While loop is useful for making an item occur once from a list");
+    } 
+       while (a<10);
+
+     { System.out.printf("%n%nMy people rejoice! It is a beautiful World, JAVA is no rocket Science, make her your LOVER");}  
+
+
+}
+
+}
